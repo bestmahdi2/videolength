00f0e47d15f44f3c1917403f5d33b6ce8228f5d6
master <alibad979@gmail.com>
2020-12-10 13:53:31 +0330

    first commit

---
+++ get info teacher.py
@@ -0,0 +1,39 @@
+from datetime import timedelta
+from os import walk, path
+from sys import stdout
+from moviepy.editor import VideoFileClip
+
+
+source = "."
+duration = 0
+types = ".mp4"
+files_count = 0
+
+def with_moviepy(filename):
+	clip = VideoFileClip(filename)
+	duration = clip.duration
+
+	return duration #, fps, (width, height)
+
+
+for dirpath, dirnames, filenames in walk(source):
+	for file in filenames:
+		if file.endswith(types):
+			files_count += 1
+
+count = 0
+for dirpath, dirnames, filenames in walk(source):
+	for file in filenames:
+		if file.endswith(types):
+			path_ = path.join(dirpath, file)
+			duration += with_moviepy(path_)
+			count += 1
+	
+		stdout.write('\r' + str("Files processed : {0}/{1}".format(count, files_count)))
+
+
+# print(duration)
+
+
+print("\nFull time: {}".format(str(int(timedelta(seconds=duration)))))
+input()
